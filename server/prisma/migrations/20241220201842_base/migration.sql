39a0565cd180d526574fb624abb98b8a89d7a857
Marcos Tobias <marcostobias64@gmail.com>
2024-12-21 00:37:33 +0100

    fix: deleted migrations

--- server/prisma/migrations/20241220201842_base/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "users" (
-    "id" SERIAL NOT NULL,
-    "name" VARCHAR(100),
-    "age" INTEGER,
-
-    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
-);
--- server/prisma/migrations/20241220203557_ayuda/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "ayuda" (
-    "id" SERIAL NOT NULL,
-    "name" VARCHAR(100),
-
-    CONSTRAINT "ayuda_pkey" PRIMARY KEY ("id")
-);
